fc92e6258740c724a9aaa8f696ffdc37bcc51494
Clayton Risen Mercer <mercerclayton19@gmail.com>
2025-10-08 17:49:36 +0800

    Update LoanCalculator.py

--- LoanCalculator.py
+++ LoanCalculator.py
@@ -1,133 +1,151 @@
 import math
 import argparse
-import sys
-import Helper
 
 
-# Store information about the command line arguments
-def command_line_parameters():
-    parser = argparse.ArgumentParser(description="This program simulates a real loan calculator.")
+# Function used to parse command line arguments
+def parse_arguments() -> list[float | int]:
+    """Parse and return the specified command line arguments as a list."""
 
-    # Loan calculator program parameters and acceptable arguments
-    parser.add_argument("--type", choices=["diff", "annuity"], help="Type of payment the user wants to calculate.")
-    parser.add_argument("--principal", help="The loan principal.")
-    parser.add_argument("--periods", help="The number of monthly payments.")
-    parser.add_argument("--interest", help="The interest rate on the loan.")
-    parser.add_argument("--payment", help="The monthly payment amount.")
+    parser = argparse.ArgumentParser()
+
+    # Arguments the user can specify
+    parser.add_argument("--type", type=str)
+    parser.add_argument("--payment", type=float)
+    parser.add_argument("--principal", type=float)
+    parser.add_argument("--periods", type=int)
+    parser.add_argument("--interest", type=float)
+
+    args = parser.parse_args()
+
+    return [args.type, args.payment, args.principal, args.periods, args.interest]
 
 
-# Calculates the required parameter of a loan according to the user's inputs
-def calculator(parameters):
-    overpayment = 0
-    if parameters.get("payment") is None:
-        principal = float(parameters.get("principal"))
-        periods = int(parameters.get("periods"))
-        interest = float(parameters.get("interest"))
-        n_interest = (interest / 100) / 12      # nominal interest = interest rate per month
+def calculate_principal(payment: float, periods: int, interest: float) -> None:
+    """
+    Print the principal amount of the loan.
+    
+    param payment: Payment to be made in each period.
+    param periods: Number of periods payment is to be made. Period is assumed to be monthly.
+    param interest: Annualized interest rate. E.g., 10 means 10%
+    """
 
-        # --type=annuity --principal=1000000 --periods=60 --interest=10
-        # Your annuity payment = 21248!
-        # Overpayment = 274880
-        if parameters.get("type") == "annuity":
-            annuity = principal * ((n_interest * (1 + n_interest) ** periods) / ((1 + n_interest) ** periods - 1))
-            overpayment = math.ceil(annuity) * periods - principal
+    check = negative_check(payment, periods, interest)
+    if check:
+        print("Incorrect parameters")
+        return
 
-            print(f"Your annuity payment = {math.ceil(annuity)}!")
+    monthly_interest = (interest * 0.01) / 12
+    numerator = monthly_interest * (1 + monthly_interest) ** periods
+    denominator = (1 + monthly_interest) ** periods - 1
+    principal = payment / (numerator / denominator)
+
+    overpayment = math.ceil((payment * periods) - principal)
+    print(f"Your loan principal = {math.ceil(principal)}!\nOverpayment = {overpayment}")
+
+
+def calculate_number_of_payments(principal: float, payment: float, interest: float) -> None:
+    """
+    Print the number of yearly and/or monthly payments required to pay off the principal.
+
+    param principal: Principal amount of the loan.
+    param payment: Payment to be made in each period. Period is assumed to be monthly.
+    param interest: Annualized interest rate. E.g., 10 means 10%
+    """
+
+    check = negative_check(principal, payment, interest)
+    if check:
+        print("Incorrect parameters")
+        return
+
+    monthly_interest = (interest * 0.01) / 12
+    x = payment / (payment - monthly_interest * principal)
+    number_of_months = math.ceil(math.log(x, 1 + monthly_interest))
+
+    years = number_of_months // 12
+    remaining_months = number_of_months % 12
+    cond1 = "years" if years > 1 else "year"
+    cond2 = "months" if remaining_months > 1 else "month"
+    if years:
+        if remaining_months:
+            print(f"It will take {years} {cond1} and {remaining_months} {cond2} to repay this loan!")
         else:
-
-            # --type=diff --principal=500000 --periods=8 --interest=7.8
-            # Month 1: payment is 65750
-            # Month 2: payment is 65344
-            # Month 3: payment is 64938
-            # Month 4: payment is 64532
-            # Month 5: payment is 64125
-            # Month 6: payment is 63719
-            # Month 7: payment is 63313
-            # Month 8: payment is 62907
-            #
-            # Overpayment = 14628
-            for month in range(1, periods + 1):
-                annuity = (principal / periods) + n_interest * (principal - ((principal * (month - 1)) / periods))
-                overpayment += math.ceil(annuity)
-                print(f"Month {month}: payment is {math.ceil(annuity)}")
-            overpayment -= principal
-
-    elif parameters.get("principal") is None:
-
-        # --type=annuity --payment=8722 --periods=120 --interest=5.6
-        # Your loan principal = 800018!
-        # Overpayment = 246622
-        annuity = float(parameters.get("payment"))
-        periods = int(parameters.get("periods"))
-        interest = float(parameters.get("interest"))
-
-        n_interest = (interest / 100) / 12
-        principal = annuity / ((n_interest * (1 + n_interest) ** periods) / ((1 + n_interest) ** periods - 1))
-        overpayment = math.ceil(annuity) * periods - principal
-
-        print(f"Your loan principal = {math.floor(principal)}!")
-
-    elif parameters.get("periods") is None:
-
-        # --type=annuity --principal=500000 --payment=23000 --interest=7.8
-        # It will take 2 years to repay this loan!
-        # Overpayment = 52000
-        principal = float(parameters.get("principal"))
-        annuity = float(parameters.get("payment"))
-        interest = float(parameters.get("interest"))
-
-        n_interest = (interest / 100) / 12
-        periods = math.ceil(math.log((annuity / (annuity - n_interest * principal)), (1 + n_interest)))
-        years = periods // 12
-        months = periods % 12
-        overpayment = periods * math.ceil(annuity) - principal
-
-        if months == 0:
-            if years > 1:
-                print(f"It will take {years} years to repay this loan!")
-            else:
-                print("It will take 1 year to repay this loan!")
-        else:
-            if years == 0:
-                if months > 1:
-                    print(f"It will take {months} months to repay this loan!")
-                else:
-                    print("It will take 1 month to repay this loan!")
-            else:
-                print(f"It will take {years} years and {months} months to repay this loan!")
-
-    print(f"Overpayment = {math.ceil(overpayment)}")
-
-
-# Retrieves command line arguments and validates them
-def get_parameters():
-    arguments = helper.extract_arguments(sys.argv)
-
-    # If --type is specified neither as "annuity" nor as "diff" or not specified at all, show the error message.
-    if arguments.get("type") not in ["diff", "annuity"]:
-        print("Incorrect parameters")
-
-    # Fewer than four parameters cannot be provided.
-    elif len(arguments) < 4:
-        print("Incorrect parameters")
-
-    # For --type=diff, a combination with --payment is invalid as payments are different every month.
-    elif arguments.get("type") == "diff" and arguments.get("payment") is not None:
-        print("Incorrect parameters")
-
-    # The loan calculator cannot calculate the interest, so it must always be provided.
-    elif arguments.get("interest") is None:
-        print("Incorrect parameters")
-
-    # Negative values cannot be entered for any parameter.
-    elif helper.check_values(arguments):
-        print("Incorrect parameters")
+            print(f"It will take {years} {cond1} to repay this loan!")
     else:
-        calculator(arguments)
+        print(f"It will take {remaining_months} {cond2} to repay the loan!")
+
+    overpayment = math.ceil((payment * number_of_months) - principal)
+    print(f"Overpayment = {overpayment}")
 
 
-command_line_parameters()
-get_parameters()
+def calculate_monthly_payment(principal: float, periods: int, interest: float, diff=True) -> None:
+    """
+    Calculate the monthly payment required to pay off the principal.
+
+    param principal: Principal amount of the loan.
+    param periods: Number of periods payment is to be made. Period is assumed to be monthly.
+    param interest: Annualized interest rate. E.g., 10 means 10%
+    """
+
+    check = negative_check(principal, periods, interest)
+    if check:
+        print("Incorrect parameters")
+        return
+
+    monthly_interest = (interest * 0.01) / 12
+    if diff:
+        total = 0
+        for month in range(1, periods + 1):
+            payment = math.ceil(
+                principal / periods + monthly_interest * (principal - (principal * (month - 1)) / periods))
+            total += payment
+            print(f"Month {month}: payment is {payment}")
+
+        overpayment = round(total - principal)
+        print(f"\nOverpayment = {overpayment}")
+    else:
+        numerator = monthly_interest * (1 + monthly_interest) ** periods
+        denominator = (1 + monthly_interest) ** periods - 1
+        annuity_payment = math.ceil(principal * (numerator / denominator))
+
+        overpayment = round(annuity_payment * periods - principal)
+        print(f"Your monthly payment = {annuity_payment}!\nOverpayment = {overpayment}")
 
 
+# Function used to check if any value in a list is negative.
+def negative_check(*args: float | int) -> bool:
+    for arg in args:
+        if arg < 0:
+            return True
+    return False
 
+
+def main():
+
+    calculation_types = ["diff", "annuity"]
+
+    arguments = parse_arguments()
+    calculation_type, payment, principal, periods, interest = arguments
+    bool_arguments = [True if x is not None else False for x in arguments]
+
+    # Input parameter validation under the following criteria:
+    # - Calculation type must be either annuity or diff;
+    # - Interest rate must be provided;
+    # - Number of input parameters must not be less than 4.
+    # - Calculation type can only be diff if calculating payment amount.
+    if calculation_type not in calculation_types or not interest or sum(bool_arguments) < 4:
+        print("Incorrect parameters")
+    elif not principal and not calculation_type == "diff":
+        calculate_principal(payment, periods, interest)
+    elif not periods and not calculation_type == "diff":
+        calculate_number_of_payments(principal, payment, interest)
+    elif not payment:
+        if calculation_type == "diff":
+            calculate_monthly_payment(principal, periods, interest)
+        else:
+            calculate_monthly_payment(principal, periods, interest, diff=False)
+    else:
+        print("Incorrect parameters")
+
+
+if __name__ == "__main__":
+    main()
